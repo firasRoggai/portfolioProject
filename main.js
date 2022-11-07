00fb890865a46359ef1bf15930943dd28cff3f41
FirasRoggai <firasreggai@gmail.com>
2022-11-07 21:46:35 +0100

    added scolling effect

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as bootstrap from "bootstrap";
 import LocomotiveScroll from 'locomotive-scroll';
-
+const mine = ()=>{
+    console.lo("test")
+}
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
