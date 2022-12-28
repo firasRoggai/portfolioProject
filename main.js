11e1d19668dfa9f53e3a2435d2fbff1a628ec62b
FirasRoggai <firasreggai@gmail.com>
2022-12-28 21:18:28 +0100

    fixed the footer issue

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 import * as bootstrap from "bootstrap";
-import LocomotiveScroll from 'locomotive-scroll';
-const mine = ()=>{
-    console.lo("test")
-}
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
