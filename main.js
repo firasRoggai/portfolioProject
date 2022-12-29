9fe3dcf45b33bb918c546fba1f3352c73daee94c
FirasRoggai <firasreggai@gmail.com>
2022-12-29 21:04:05 +0100

    added final touch

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import * as bootstrap from "bootstrap";
+import 'animate.css';
+// animation:
+const header = document.querySelector(".hJS")
+let i = 0;
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(element => {
+        i++
+        if(i<=2){
+        element.target.classList.toggle("animate__fadeInUp")
+    }
+     })
+},{
+    margin: 5
+})
+observer.observe(header)
